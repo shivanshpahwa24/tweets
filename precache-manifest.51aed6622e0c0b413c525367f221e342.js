75234e1162dd87a908e5f4f8fb682a3af0c378ab
shivansh_pahwa <shivanshpahwa24@gmail.com>
2019-11-29 18:20:29 +0530

    Updates

---
+++ precache-manifest.51aed6622e0c0b413c525367f221e342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3694a9d49632ab3c62767931adc356c0",
+    "url": "/tweets/index.html"
+  },
+  {
+    "revision": "859be9aac344e429f030",
+    "url": "/tweets/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "8906630a90f6d3ee1e9f",
+    "url": "/tweets/static/css/main.230c7bd4.chunk.css"
+  },
+  {
+    "revision": "859be9aac344e429f030",
+    "url": "/tweets/static/js/2.a8181fa5.chunk.js"
+  },
+  {
+    "revision": "8906630a90f6d3ee1e9f",
+    "url": "/tweets/static/js/main.aba00dc5.chunk.js"
+  },
+  {
+    "revision": "d0d0f1839dccb8ed6f37",
+    "url": "/tweets/static/js/runtime-main.e540074c.js"
+  }
+]);
